57862ddb58ac4422339bb384f1d544f828b3e620
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2025-05-12 09:15:32 +0200

    feat: parametrized image resize

--- src/utils/images.py
+++ src/utils/images.py
@@ -2,30 +2,29 @@ from PIL import Image, ImageFile
 from io import BytesIO
 import base64
 
+MAX_HEIGHT = 720
+MAX_WIDTH = 1280
+
 
 # Convert Image to Base64
 def im_2_b64(image: Image.Image) -> str:
     buff = BytesIO()
     aspect_ratio = image.size[0] / image.size[1]
-    max_height = 720
-    max_width = 1280
-    if image.size[0] > max_width:
-        image = image.resize((max_width, int(max_width / aspect_ratio)))
-    if image.size[1] > max_height:
-        image = image.resize((int(max_height * aspect_ratio), max_height))
+    if image.size[0] > MAX_WIDTH:
+        image = image.resize((MAX_WIDTH, int(MAX_WIDTH / aspect_ratio)))
+    if image.size[1] > MAX_HEIGHT:
+        image = image.resize((int(MAX_HEIGHT * aspect_ratio), MAX_HEIGHT))
     image.convert("RGB").save(buff, format="JPEG")
     img_str = base64.b64encode(buff.getvalue()).decode("utf-8")
     return img_str
 
 
-def resize_img(image):
+def resize_img(image, new_width=MAX_WIDTH, new_height=MAX_HEIGHT) -> Image.Image:
     aspect_ratio = image.size[0] / image.size[1]
-    max_height = 720
-    max_width = 1280
-    if image.size[0] > max_width:
-        image = image.resize((max_width, int(max_width / aspect_ratio)))
-    if image.size[1] > max_height:
-        image = image.resize((int(max_height * aspect_ratio), max_height))
+    if image.size[0] > new_width:
+        image = image.resize((new_width, int(new_width / aspect_ratio)))
+    if image.size[1] > new_height:
+        image = image.resize((int(new_height * aspect_ratio), new_height))
     return image
 
 
